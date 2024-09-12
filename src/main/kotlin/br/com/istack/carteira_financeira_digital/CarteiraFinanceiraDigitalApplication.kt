acf90753744c1c888b9d1113e9f31e7548b1ec34
Raífe <raife.tec@gmail.com>
2024-09-11 22:14:39 -0300

    projeto base

---
+++ src/main/kotlin/br/com/istack/carteira_financeira_digital/CarteiraFinanceiraDigitalApplication.kt
@@ -0,0 +1,11 @@
+package br.com.istack.carteira_financeira_digital
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarteiraFinanceiraDigitalApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarteiraFinanceiraDigitalApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/istack/carteira_financeira_digital/CarteiraFinanceiraDigitalApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.istack.carteira_financeira_digital
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarteiraFinanceiraDigitalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
