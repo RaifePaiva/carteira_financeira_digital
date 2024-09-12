24cd8f17c86ed9e50f97271d23b81d1dd019d559
Raífe <raife.tec@gmail.com>
2024-09-11 22:26:42 -0300

    Incluindo plugin jacoco

--- src/test/kotlin/br/com/istack/carteira_financeira_digital/CarteiraFinanceiraDigitalApplicationTests.kt
+++ src/test/kotlin/br/com/istack/carteira_financeira_digital/CarteiraFinanceiraDigitalApplicationTests.kt
@@ -1,6 +1,9 @@
 package br.com.istack.carteira_financeira_digital
 
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.springframework.boot.runApplication
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -8,6 +11,15 @@ class CarteiraFinanceiraDigitalApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		val context = runApplication<CarteiraFinanceiraDigitalApplication>()
+		assertNotNull(context)
+	}
+
+	@Test
+	fun `main method runs without exceptions`() {
+		assertDoesNotThrow {
+			main(emptyArray())
+		}
 	}
 
 }
