be2d88c19a216d154db4ca23a18f3710969a8e14
Raífe <raife.tec@gmail.com>
2024-09-11 22:42:02 -0300

    Incluindo plugin jacoco

--- src/test/kotlin/br/com/istack/carteira_financeira_digital/CarteiraFinanceiraDigitalApplicationTests.kt
+++ src/test/kotlin/br/com/istack/carteira_financeira_digital/CarteiraFinanceiraDigitalApplicationTests.kt
@@ -1,27 +1,13 @@
 package br.com.istack.carteira_financeira_digital
 
-import org.junit.jupiter.api.Assertions.assertDoesNotThrow
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-import org.springframework.boot.runApplication
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class CarteiraFinanceiraDigitalApplicationTests {
 
 	@Test
 	fun contextLoads() {
-		val context = runApplication<CarteiraFinanceiraDigitalApplication>()
-		assertNotNull(context)
-	}
-
-	@Test
-	fun `main method runs without exceptions`() {
-		assertDoesNotThrow {
-			main(emptyArray())
-		}
 	}
 
 }
